13f8706b8ecc1605008dd79ec8ab68e3947fcfc5
maryrine onsare <maryrine.onsare@student.moringaschool.com>
2022-06-05 15:12:56 +0300

    commit tree 413e244c5177fa88de48f745ff91b5ccfb11a390

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    
+}
+function appendCat(name){
+    let newArr=[...cats];
+
+    newArr.push(name);
+    return newArr;
+}
+function prependCat(name){
+    let newArr=[...cats];
+
+    newArr.unshift(name);
+    return newArr;
+}
+
+function removeLastCat() {
+    let newArr=[...cats];
+
+    newArr.pop();
+    return newArr;
+    
+}
+function removeFirstCat() {
+    let newArr=[...cats]
+    newArr.shift()
+    return newArr
+}
+
